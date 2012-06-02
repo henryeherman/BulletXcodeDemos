354943fcc6a6f2df53279ec8266d840566502ef8
Jason Tsao <jtsao22@gmail.com>
2012-06-01 18:41:22 -0700

    Hexapod is walking perfectlygs

--- hexapod/Leg.cpp
+++ hexapod/Leg.cpp
@@ -48,11 +48,11 @@ Leg::Leg (Hexapod *hexapod, btDynamicsWorld* ownerWorld, const btTransform& offs
     btQuaternion temp;
     
 	//transform.setOrigin(btVector3(btScalar(-0.18*scale_hexapod), btScalar(0.65*scale_hexapod),                                  btScalar(0.)));
-	m_bodies[LEG_UPPER] = localCreateRigidBody(btScalar(1.), globalFrame*transform, m_shapes[LEG_UPPER]);
+	m_bodies[LEG_UPPER] = localCreateRigidBody(btScalar(5.), globalFrame*transform, m_shapes[LEG_UPPER]);
     
 	transform.setIdentity();
 	transform.setOrigin(btVector3(btScalar(0*scale_hexapod), btScalar((-1)*LOWER_LEG_LENGTH*scale_hexapod), btScalar(0.)));
-	m_bodies[LEG_LOWER] = localCreateRigidBody(btScalar(1.), globalFrame*transform, m_shapes[LEG_LOWER]);
+	m_bodies[LEG_LOWER] = localCreateRigidBody(btScalar(5.), globalFrame*transform, m_shapes[LEG_LOWER]);
 
     
 	// Setup some damping on the m_bodies
---
+++ hexapod/hexapodcontroller/walkingdemo.py
@@ -0,0 +1,69 @@
+#/usr/bin/env python
+
+import time
+import hexapod
+from hexapod import HpodSimCtrlParam
+import numpy as np
+import sys
+
+pod = hexapod.Hexapod()
+
+pod.kneeStrength=100
+pod.hipStrength=30
+pod.dtKnee = 0.1
+pod.dtHip = 0.1
+
+t = np.arange(0,50,0.01)
+w = 1
+xs = np.sin(2*np.pi*t/1)
+xc = np.cos(2*np.pi*t/1)
+
+xs_half = -(np.sin(1*np.pi*t/1))
+xc_half = -(np.cos(1*np.pi*t/1))
+
+pod.setControl(HpodSimCtrlParam.RESETEXP)
+pod.send()
+pod.setControl(HpodSimCtrlParam.CONTINUE)
+pod.send()
+
+pod.setControl(HpodSimCtrlParam.LOAD)
+
+print "Begin Exp"
+try:
+    print "Build Array"
+    for pos_index, sin_pos in enumerate(xs[::1]):
+
+        cos_pos = xc[pos_index]
+        sin_pos_half = xs_half[pos_index]
+        cos_pos_half = xc_half[pos_index]
+
+        for leg_index, leg in enumerate(pod.legs):
+
+            # Control FRONT_LEFT, MIDDLE_RIGHT, and BACK_LEFT legs
+            if leg_index in [0, 3, 4]:
+                leg.knee.angle= np.pi/2
+                leg.hip.yangle= -np.pi/6 * sin_pos
+                leg.hip.xangle= np.pi/6 * abs(sin_pos_half)
+
+            # Control FRONT_RIGHT, MIDDLE_LEFT, and BACK_RIGHT legs
+            else:
+                leg.knee.angle= np.pi/2
+                leg.hip.yangle= np.pi/6 * cos_pos
+                leg.hip.xangle= np.pi/6 * abs(cos_pos_half)
+
+        pod.addParam()
+
+        print "Add Param %f" % sin_pos
+    print "Array Built... sending"
+    pod.sendArray()
+    pod.clearParamArray()
+    pod.setControl(HpodSimCtrlParam.RUNEXP)
+    pod.send()
+    pod.clearParamArray()
+
+    print "Sent %d packets" % len(xs)
+
+except (KeyboardInterrupt,):
+
+    print "Exit Hexapod Client"
+    sys.exit(0)
